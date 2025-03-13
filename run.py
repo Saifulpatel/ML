4d047289bf51245b67f1452e86ed54c7f55a5005
Saifulpatel <119802436+Saifulpatel@users.noreply.github.com>
2025-03-13 12:53:09 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,4 @@ from app import create_app
 app, port = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render will assign a PORT dynamically
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=10000)
